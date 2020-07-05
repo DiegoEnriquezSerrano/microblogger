24b8984ddb78026bd32b68354d85e21164adfdb9
diego enriquez-serrano <diego@provi.com>
2020-07-05 12:18:08 -0500

    added created_at columns and updated api calls

--- app/api/follow.php
+++ app/api/follow.php
@@ -2,12 +2,19 @@
 
 require_once "../../functions.php";
 
-  $result = bind_and_get_result("SELECT * FROM following_relations WHERE follower = ? AND is_following = ?", "ss", $new=array(esc($_SESSION['id']),esc($_POST['userid'])));
-  if (mysqli_num_rows($result) > 0) {
-    $row = fetch_assoc($result);
-    bind_and_execute_stmt("DELETE FROM following_relations WHERE id = ?", "s", esc($row['id']));
-    echo 1;
-  } else {
-    bind_and_execute_stmt("INSERT INTO following_relations (follower, is_following) VALUES ( ?, ?) ", "ss", $new=array(esc($_SESSION['id']),esc($_POST['userid'])));
-    echo 2;
-  }
+$result = bind_and_get_result(
+  "SELECT * FROM following_relations WHERE follower = ? AND is_following = ?", "ss",
+  [ esc($_SESSION['id']), esc($_POST['userid']) ]
+);
+if (mysqli_num_rows($result) > 0) {
+  $row = fetch_assoc($result);
+  bind_and_execute_stmt("DELETE FROM following_relations WHERE id = ?", "s", esc($row['id']));
+  echo 1;
+} else {
+  $date = date('Y-m-d H:i:s');
+  bind_and_execute_stmt(
+    "INSERT INTO following_relations (follower, is_following, created_at) VALUES ( ?, ?, ?) ", "sss",
+    [ esc($_SESSION['id']), esc($_POST['userid']), $date ]
+  );
+  echo 2;
+}
--- app/api/like.php
+++ app/api/like.php
@@ -2,13 +2,14 @@
 
 require_once "../../functions.php";
 
-
-$result = bind_and_get_result("SELECT * FROM liked_relations WHERE user = ? AND post_liked = ?", "ss", $new=array(esc($_SESSION['id']),esc($_POST['postid'])));
+$result = bind_and_get_result("SELECT * FROM liked_relations WHERE user = ? AND post_liked = ?", "ss", [esc($_SESSION['id']),esc($_POST['postid'])]);
 if (mysqli_num_rows($result) > 0) {
   $row = fetch_assoc($result);
   bind_and_execute_stmt("DELETE FROM liked_relations WHERE id = ?", "s", esc($row['id']));
   echo 1;
 } else {
-  bind_and_execute_stmt("INSERT INTO liked_relations (user, post_liked) VALUES ( ?, ?) ", "ss", $new=array(esc($_SESSION['id']),esc($_POST['postid'])));
+  $date = date('Y-m-d H:i:s');
+  bind_and_execute_stmt("INSERT INTO liked_relations (user, post_liked, created_at) VALUES ( ?, ?, ?) ", "sss",
+    [ esc($_SESSION['id']), esc($_POST['postid']), $date ]);
   echo 2;
 }
