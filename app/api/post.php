2165af6de626ef91a03bae6bec81a2f0f33a3f41
diego enriquez-serrano <diego@provi.com>
2020-07-05 10:31:15 -0500

    fix regular post error

--- app/api/post.php
+++ app/api/post.php
@@ -3,11 +3,19 @@
 require_once "../../functions.php";
 
 if (isset($_POST)) {
+  
   $json = json_decode(file_get_contents('php://input'), true);
   $json['draft'] == false ? $info = ['posts', 'postid='] : $info = ['drafts', 'draftid='];
   $date = date('Y-m-d H:i:s');
+
   if (!isset($json['text'])) exit("Cannot create empty {$info[0]}");
   if (strlen($json['text']) > 69420) exit("Post is too long.");
+
+  if (!isset($json['isRepost'])) {
+    $json['isRepost'] = 0;
+    $json['repostFromPostId'] = null;
+  };
+
   if (isset($json['draftId']) && $json['draft'] == true) {
     bind_and_execute_stmt(
       "UPDATE drafts SET post = ?, datetime = ?
@@ -31,4 +39,5 @@ if (isset($_POST)) {
       );
     };
   };
+
 };
